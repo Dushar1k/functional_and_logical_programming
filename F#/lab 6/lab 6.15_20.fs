2f3b5ddcfac066ed979c84336179b0ea63759c26
Dushar1k <larik200104@gmail.com>
2022-04-23 10:08:20 +0300

    add lab 6.15

---
+++ F#/lab 6/lab 6.15_20.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+// Function that allows the user to enter a list from the console
+let list_input =
+    let rec l_i list =
+        let input = Convert.ToInt32(Console.ReadLine())
+        if input = 0 then list
+        else l_i (list@[input])
+    l_i []
+
+let find_missed_int list =
+    let rec fmi list list1 i =
+        match list with
+        | [] -> list1
+        | H::T -> if H = i then fmi T list1 (i+1) else fmi list (list1@[i]) (i+1)
+    fmi list [] list.Head
+
+[<EntryPoint>]
+let main args =
+    let list = list_input
+    printfn "%A" (find_missed_int list)
+    0
