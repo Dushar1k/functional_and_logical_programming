cdb199168d5b5dde5d1b58197903820b1a143993
Dushar1k <larik200104@gmail.com>
2022-04-23 10:22:47 +0300

    add lab 6.16

---
+++ F#/lab 6/lab 6.16_27.fs
@@ -0,0 +1,22 @@
+﻿open System
+
+// Function that allows the user to enter a list from the console
+let list_input =
+    let rec l_i list =
+        let input = Convert.ToInt32(Console.ReadLine())
+        if input = 0 then list
+        else l_i (list@[input])
+    l_i []
+
+let rec cyclic_shift_l list n =
+    if n = 0 then list
+    else
+        match list with
+        | H::T -> cyclic_shift_l (T@[H]) (n-1)
+        | [] -> list
+
+[<EntryPoint>]
+let main args = 
+    let list = list_input
+    printfn "%A" (cyclic_shift_l list 1)
+    0
