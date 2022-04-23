8b239b14d047fa659b4bb8a68f1caf23ff894e38
Dushar1k <larik200104@gmail.com>
2022-04-23 16:14:50 +0300

    add comment

--- F#/lab 6/lab 6.20_54.fs
+++ F#/lab 6/lab 6.20_54.fs
@@ -8,15 +8,17 @@ let list_input =
         else l_i (list@[input])
     l_i []
 
+// Looks for the element that occurs most often
 let number_of_times list n = (List.filter(fun x -> x = n) list).Length
 
+// Creates a list consisting of the indexes at which the most frequent element is located
 let f list =
     let rec cr_l list list2 =
         match list with
         | H::T -> if number_of_times list H > 3 && not(List.exists(fun x -> x = H) list2) then cr_l T (list2@[H]) else cr_l T list2
         | [] -> list2
     cr_l list []
-    
+
 [<EntryPoint>]
 let main args = 
     let list = list_input
