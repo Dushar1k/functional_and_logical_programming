456ea92df8186a6f7c9db908fc6231414d3ca272
Dushar1k <larik200104@gmail.com>
2022-05-10 10:51:41 +0300

    modded lab 6.17_30.fs

--- F#/lab 6/lab 6.17_30.fs
+++ F#/lab 6/lab 6.17_30.fs
@@ -8,18 +8,25 @@ let list_input =
         else l_i (list@[input])
     l_i []
 
+let length list =
+    let rec len list n =
+        match list with
+        | [] -> n
+        | H::T -> len T (n + 1)
+    len list 0
+
 // Searches for the element with the given index
 let rec list_ind (list: int list) i =
-    if i >= list.Length then 0
+    if i >= (length list) then 0
     else
         match list with
         | H::T -> if i = 0 then H else list_ind T (i-1)
 
 // Checks if the element at the specified index is a local maximum
 let is_local_max (list:int list) i =
-    if list.Length > (i-1) && i > 0 && list_ind list i >= list_ind list (i+1) && list_ind list i >= list_ind list (i-1) then true
+    if length list > (i-1) && i > 0 && list_ind list i >= list_ind list (i+1) && list_ind list i >= list_ind list (i-1) then true
     elif i = 0 && list_ind list 0 >= list_ind list 1 then true
-    elif i = list.Length && list_ind list i >= list_ind list (i-1) then true
+    elif i = length list && list_ind list i >= list_ind list (i-1) then true
     else false
 
 [<EntryPoint>]
