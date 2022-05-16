50354c8edcb79baca522032301a75219b4bf18a1
Dushar1k <larik200104@gmail.com>
2022-05-16 19:38:55 +0300

    change recursion

--- Prolog/lab 11/lab 11_16.pl
+++ Prolog/lab 11/lab 11_16.pl
@@ -108,10 +108,11 @@ grand_sons(X):- parent(X,Y), parent(Y,Z), man(Z), write(Z), write(", "), fail.
 
 grand_ma_and_da(X,Y):- woman(X), woman(Y), ((parent(X,Z), parent(Z,Y)); (parent(Y,Z), parent(Z,X))), !.
 
-min_up(0,9):- !.
-min_up(X,N):- X1 is X div 10, Cur is X mod 10, min_up(X1,N1), ((N1 < Cur, N is N1); N is Cur), !.
+min_up(0,N,N):- !.                                            min_up(X,N,Min):- Cur is X mod 10, X1 is X div 10, ((Cur < Min
+, min_up(X1,N,Cur)); min_up(X1,N,Min)), !.
+min_up(X,N):- Cur is X mod 10, min_up(X,N,Cur), !.
 
-min_down(0,N,N):- !.
-min_down(X,N,Min):- Cur is X mod 10, X1 is X div 10, ((Cur < Min, min_down(X1,N,Cur)); min_down(X1,N,Min)), !.
-min_down(X,N):- Cur is X mod 10, min_down(X,N,Cur), !.
+min_down(0,9):- !.                                            min_down(X,N):- X1 is X div 10, Cur is X mod 10, min_down(X1,N
+1), ((N1 < Cur, N is N1); N is Cur), !.
+
 
