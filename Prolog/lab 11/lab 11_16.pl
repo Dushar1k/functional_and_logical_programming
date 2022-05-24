012a15d6025a19f23fab7c06c4e991287677cc8a
Dushar1k <larik200104@gmail.com>
2022-05-24 16:51:57 +0300

    code formating

--- Prolog/lab 11/lab 11_16.pl
+++ Prolog/lab 11/lab 11_16.pl
@@ -112,8 +112,7 @@ min_up(0,9):- !.
 min_up(X,N):- X1 is X div 10, Cur is X mod 10, min_up(X1,N1), ((N1 < Cur, N is N1); N is Cur), !.
 
 min_down(0,N,N):- !.
-min_down(X,N,Min):- Cur is X mod 10, X1 is X div 10, ((Cur < Min
-, min_down(X1,N,Cur)); min_down(X1,N,Min)), !.
+min_down(X,N,Min):- Cur is X mod 10, X1 is X div 10, ((Cur < Min, min_down(X1,N,Cur)); min_down(X1,N,Min)), !.
 min_down(X,N):- Cur is X mod 10, min_down(X,N,Cur), !.
 
 
